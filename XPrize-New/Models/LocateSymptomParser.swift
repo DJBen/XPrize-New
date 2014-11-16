e01206909b6e4c5d3fb4324e8343c7d4141c4b2b
DJBen <lsh32768@gmail.com>
2014-11-16 15:36:28 -0500

    Redo locate symptoms

---
+++ XPrize-New/Models/LocateSymptomParser.swift
@@ -0,0 +1,13 @@
+//
+//  LocateSymptomParser.swift
+//  XPrize-New
+//
+//  Created by Sihao Lu on 11/9/14.
+//  Copyright (c) 2014 DJ.Ben. All rights reserved.
+//
+
+import UIKit
+
+class LocateSymptomParser: NSObject {
+   
+}
---
+++ XPrize-New/Views/PersonBodyPartView.swift
@@ -0,0 +1,21 @@
+//
+//  PersonBodyPartView.swift
+//  XPrize-New
+//
+//  Created by Sihao Lu on 11/16/14.
+//  Copyright (c) 2014 DJ.Ben. All rights reserved.
+//
+
+import UIKit
+
+class PersonBodyPartView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
